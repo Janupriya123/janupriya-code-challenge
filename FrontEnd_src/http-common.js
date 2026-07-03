1c6f31afa7df71c8c91b8b0ea4f6aef21f9d09ce
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2026-07-03 12:26:45 +0530

    FrontEnd Code Challenge

--- FrontEnd_src/http-common.js
+++ FrontEnd_src/http-common.js
@@ -2,6 +2,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "http://localhost:8081/api",
     headers: {
-        "Content-Type":"application/json"
+        "Content-Type":"application/json" 
     }
 });
