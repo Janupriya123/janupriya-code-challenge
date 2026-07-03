b955a73b4f25ca5c842cdb7ffec03a497ce56301
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2026-07-03 12:30:27 +0530

    FrontEnd Code Challenge

--- FrontEnd_src/components/Navbar.js
+++ FrontEnd_src/components/Navbar.js
@@ -1,36 +1,55 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
-function Navbar() { 
+function Navbar() {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Container>
-        <Navbar.Brand as={Link} to="/">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
           Cricket Team Management
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse id="navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
-              Home
-            </Nav.Link>
-            <Nav.Link as={Link} to="/players">
-              Player List
-            </Nav.Link>
-            <Nav.Link as={Link} to="/add-player">
-              Add Player
-            </Nav.Link>
-            <Nav.Link as={Link} to="/get-player">
-              Get Player
-            </Nav.Link>
-            <Nav.Link as={Link} to="/get-player-by-team">
-              Get Player By Team
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/players">
+                Player List
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/add-player">
+                Add Player
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/get-player">
+                Get Player
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/get-player-by-team">
+                Search By Team
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </nav>
   );
 }
 
